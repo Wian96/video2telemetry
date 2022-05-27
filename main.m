4b5ee63a3371f3854cadf6749e55be1f5bd1f41d
Wian96 <sebastiano96pn@gmail.com>
2022-05-27 10:35:01 +0200

    Committed some modifications

--- main.m
+++ main.m
@@ -1,5 +1,28 @@
 close all; clear; clc
 
+%% GENERATE THE tmp DIRECTORY
+
+mkdir __tmp_img__
+mkdir __dat_out__
+
+%% RUN THE ITERATION ON THE VIDEO
+
+vid = VideoReader('video.mp4');
+len = vid.Duration; % Get video length
+width = vid.Width; % Get video width
+height = vid.Height; % Get video height
+fr = vid.FrameRate; % Get framerate
+% Intialize the time vector based on video length
+time_vect = linspace(0,len,len*fr);
+
+for ii = 1:length(time_vect)
+    vid.CurrentTime = ii; %Suppose We have a 50 s video, by using this command we can set at 36 seconds. 
+    vidFrame = read(vid,ii); %This extract the exact frame of the video (in our case a graph at a specific point) 
+    out_str = ['__tmp_img__\frame_'+string(ii)+'.png'];
+    imwrite(vidFrame,out_str);
+    % Run python function
+    pyrunfile('__main__.py',out_str);
+end
 % Get data
 data_loaded = load('output.dat');
 % Sort from lower to higher on x-axis
